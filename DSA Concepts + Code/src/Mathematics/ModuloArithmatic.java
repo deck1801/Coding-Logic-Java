32ce8f7bbf2bfe7ab7c61995ec128a6c207ea0b3
Divyajyoti <divyajyoti182000@gmail.com>
2022-07-21 16:26:39 +0530

    Day-4

--- DSA Concepts + Code/src/Mathematics/ModuloArithmatic.java
+++ DSA Concepts + Code/src/Mathematics/ModuloArithmatic.java
@@ -2,6 +2,31 @@ package Mathematics;
 
 public class ModuloArithmatic {
     public static void main(String args[]){
-        
+        System.out.println(FastPower(3, 5));
+    }
+    
+    public static int FastPower(int n,int b){
+        int res=1;
+        while(b>0){
+            if((b&1)!=0){ // replacement of b%2!=0
+                res*=n;
+            }
+            n=n*n;
+            b=b>>1; //replacement of b/2
+        }
+        return res;
+    }
+    
+    static long LargeFastPower(long n,long b,int m){
+        long res=1;
+        while(b>0){
+            if((b&1)!=0){
+                res=(res*n % m)% m;
+            }
+            n=(n%m * n%m)%m;
+            b=b>>1;
+        }
+        return res;
+
     }
 }
